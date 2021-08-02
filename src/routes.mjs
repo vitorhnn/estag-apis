c14902b3bb1c30087ca2ba8febd4616e49982d23
Victor Chiletto <victor.chiletto@b2wdigital.com>
2021-08-02 11:17:28 -0300

    Cria rota para criar um produto

--- src/routes.mjs
+++ src/routes.mjs
@@ -7,4 +7,28 @@ router.get("/products", (req, res) => {
     res.send("Hi!");
 });
 
+// entrada dessa rota:
+/*
+{
+    "name": ...,
+    "price" ...,
+    "weight": ...,
+}
+*/
+router.post("/products", async (req, res) => {
+    const { name, price, weight } = req.body;
+
+    if (isNaN(parseFloat(price))) {
+        return res.status(400).json({ msg: "price não era número" });
+    }
+
+    if (isNaN(parseFloat(weight))) {
+        return res.status(400).json({ msg: "weight não era número" });
+    }
+
+    const id = await db.insert({ name, price, weight }).into("products").returning("id");
+
+    res.status(201).json({ id, name, price, weight });
+});
+
 export default router;
